3d91c03e8beedbdd7187ce15f85447bbb8933304
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-03-17 22:39:11 +0200

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { persistor, store } from 'redux/store';
 import { App } from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
   // <React.StrictMode>
   <BrowserRouter basename="/Templates-REACT">
     <Provider store={store}>
