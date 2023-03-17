fcb85e0b422fddec0b6eca47f39a9a9afa6a7045
jekiekolya <108493349+jekiekolya@users.noreply.github.com>
2023-03-17 20:40:19 +0000

    Deploying to gh-pages from @ jekiekolya/Templates-TS-React@3d91c03e8beedbdd7187ce15f85447bbb8933304 🚀

---
+++ static/js/256.9419e09d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktemplates_ts_react=self.webpackChunktemplates_ts_react||[]).push([[256],{256:function(t,e,n){n.r(e),n.d(e,{default:function(){return s}});var r=n(329);function s(){return(0,r.jsx)("h2",{style:{color:"red"},children:"NotFound"})}}}]);
+//# sourceMappingURL=256.9419e09d.chunk.js.map
